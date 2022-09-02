126d1c2eb3203364a47a4b5fcfc5262caa047cb6
omayed <password janina>
2022-09-03 01:10:11 +0600

    initial commit

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,25 @@
+<template>
+   <div class="container">
+      <div class="min-vh-100 d-flex justify-content-center align-items-center row">
+         <div class="col-4">
+            <select class="form-select form-control" aria-label="Default select example" name="" id="">
+            <option value="">rbtr</option>
+            <option value="">trbr</option>
+            <option value="">brtbr</option>
+            <option value="">btrbtr</option>
+         </select>
+         </div>
+      </div>
+
+   </div>
+</template>
+
+<script>
+    export default {
+        name: "drop-down"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import dropdown from './components/Dropdown.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 
-createApp(App).mount('#app')
+
+
+const app = createApp(App);
+app.component('drop-down', dropdown).mount('#app')
